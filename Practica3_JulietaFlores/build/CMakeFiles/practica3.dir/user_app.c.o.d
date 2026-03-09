d3383048beb84c5bd9e539133f1c8d4acce3c56e
julietanilem <julietanilem@gmail.com>
2026-03-08 22:44:52 -0600

    prac3

---
+++ Practica3_JulietaFlores/build/CMakeFiles/practica3.dir/user_app.c.o.d
@@ -0,0 +1,10 @@
+CMakeFiles/practica3.dir/user_app.c.o: \
+ /home/julietanilem/sexto_semestre/sistemas_operativos/PracticasSO/Practica3_JulietaFlores/user_app.c \
+ /usr/lib/gcc/arm-none-eabi/15.2.0/include/stdint.h \
+ /usr/arm-none-eabi/include/stdint.h \
+ /usr/arm-none-eabi/include/machine/_default_types.h \
+ /usr/arm-none-eabi/include/sys/features.h \
+ /usr/arm-none-eabi/include/_newlib_version.h \
+ /usr/arm-none-eabi/include/sys/_intsup.h \
+ /usr/arm-none-eabi/include/sys/_stdint.h \
+ /home/julietanilem/sexto_semestre/sistemas_operativos/PracticasSO/Practica3_JulietaFlores/syscalls.h
