42107de402120eed7167a84f1606b0534ea8cb6b
julietanilem <julietanilem@gmail.com>
2026-03-22 17:08:58 -0600

    Fase A

---
+++ Practica4_JulietaFlores/build/_deps/picotool-build/data_locs.cpp
@@ -0,0 +1,7 @@
+
+#include <vector>
+#include <string>
+
+std::vector<std::string> data_locs = {
+    "./","/home/julietanilem/sexto_semestre/sistemas_operativos/PracticasSO/Practica4_JulietaFlores/build/_deps/picotool/"
+};
